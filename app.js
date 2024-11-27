0470d4ee6ba76072de8e2f5e97b24f7cded234c7
huanln <ngochuancntt4@gmail.com>
2024-11-28 00:07:27 +0700

    ok

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get(`/`, function(req, res){
     res.send('Hello World');
 })
 
-app.listen(process.env.PORT, function(){
+let port = process.env.PORT || 8080;
+app.listen(port, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
