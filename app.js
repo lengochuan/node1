fe8d47a01be0de2d7d23b06b3833d9b3ffc77a89
huanln <ngochuancntt4@gmail.com>
2024-11-27 23:40:48 +0700

    simple

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.get(`/home`, function(req, res){
+app.get(`/`, function(req, res){
     res.send('Hello World');
 })
 
